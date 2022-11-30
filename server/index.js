5dc0c73b7a94b52c67800e5e70cd3f86a4b1e343
Nicola <nicola.bonan@stud.itsaltoadriatico.it>
2022-11-30 15:43:14 +0100

    creazione file server.js per protocollo http

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-// var restify = require('restify');
-// var errs = require('restify-errors');
+
 var database = require('../server/database/db.js')
 var mqtt = require('mqtt')
 const brokerUrl = "localhost://127.0.0.1:1883"
@@ -54,41 +53,3 @@ client.on('error', function(error){
 })
 
 
-//apro la connessione con il db direttamente da qua
-// var server = restify.createServer();
-// server.use(restify.plugins.bodyParser());
-
-// server.get('/drones', function (req, res, next) {
-//     res.send('List of drones: [TODO]');
-//     return next();
-// });
-
-// server.get('/drones/:id', function (req, res, next) {
-//     res.send('Current values for drone ' + req.params['id'] + ': [TODO]');
-//     return next();
-// });
-
-// server.post('/drones/:id', async function (req, res, next) {
-
-//     if(req.body)
-//     {
-//         var drone = JSON.parse(req.body);
-//         await database.postDrone(drone)
-//         res.send(drone);
-//         res.status(200, 'Ok')
-//         console.log(drone);
-//         console.log("---------------------------------")
-//     }
-//     else
-//     {
-//         err =  next(errs.BadRequestError())
-//         console.log(err)
-//         return err
-//     }
-    
-//     return next();
-// });
-
-// server.listen(8011, function (req, res, next) {
-//     console.log('%s listening at %s', server.name, server.url);
-// });
