97206a7e098a72623f739adfea396e26e2e97be6
andrea200113 <afinco2001@gmail.com>
2022-12-07 15:05:39 +0100

    Creazione classe amqp

---
+++ client/NetCoreClient/Protocols/Amqp.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace NetCoreClient.Protocols
+{
+    public class Amqp : Interfacce.IProtocol
+    {
+
+        private string? _hostName;
+
+        private ConnectionFactory? _client;
+        private IConnection? _conn;
+        //private IModel? _channel;
+        public Amqp(string? hostName)
+        {
+            _hostName = hostName;
+            _client = new ConnectionFactory()
+            {
+                HostName = _hostName,
+            };
+            _conn = _client.CreateConnection();
+        }
+
+
+        public void Send(string data)
+        {
+            using (var channel = _conn?.CreateModel())
+            {
+                channel.QueueDeclare(
+                        queue: "prova",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false
+                    );
+
+                var body = Encoding.UTF8.GetBytes(data);
+
+                channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "droni",
+                        body: body
+                    );
+            }
+        }
+    }
+}
